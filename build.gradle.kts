01a03ee4e89f8f7909af7438d98b46843263f0dc
03674738 <cyh222@skuniv.ac.kr>
2026-04-20 10:16:19 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:4.0.1")
     implementation("org.springframework.security:spring-security-crypto")
-    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.3.0")
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:4.0.1")
 
     runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
 
