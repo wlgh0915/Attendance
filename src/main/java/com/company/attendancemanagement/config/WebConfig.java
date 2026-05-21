7542028e3a863b3cb60aa196163c979097f20b80
03674738 <cyh222@skuniv.ac.kr>
2026-05-21 17:04:39 +0900

    0521 17:04 접근 권한 수정중

--- src/main/java/com/company/attendancemanagement/config/WebConfig.java
+++ src/main/java/com/company/attendancemanagement/config/WebConfig.java
@@ -1,4 +1,20 @@
 package com.company.attendancemanagement.config;
 
-public class WebConfig {
+import com.company.attendancemanagement.interceptor.MenuPermissionInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final MenuPermissionInterceptor menuPermissionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(menuPermissionInterceptor)
+                .addPathPatterns("/**");
+    }
 }
